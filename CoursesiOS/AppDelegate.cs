1eba9710c014adad03c664c427990e00e2d30bb2
Craig Carey <craig@7tech.co.uk>
2015-07-27 14:25:28 +0100

    Initial commit

---
+++ CoursesiOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace CoursesiOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+		CourseViewController viewController;
+
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+			viewController = new CourseViewController ();
+			Window.RootViewController = viewController;
+
+			Window.MakeKeyAndVisible ();
+
+			return true;
+		}
+    }
+}
