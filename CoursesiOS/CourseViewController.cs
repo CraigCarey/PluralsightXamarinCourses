a4ede869a3b9721623567987ff003cfeb582b3f7
Craig Carey <sgccarey@gmail.com>
2015-07-27 15:20:18 +0100

    Added CoursesAndroid project

--- CoursesiOS/CourseViewController.cs
+++ CoursesiOS/CourseViewController.cs
@@ -28,17 +28,20 @@ namespace CoursesiOS
 
             buttonPrev.TouchUpInside += buttonPrev_TouchUpInside;
             buttonNext.TouchUpInside += buttonNext_TouchUpInside;
-
 		}
 
         private void buttonPrev_TouchUpInside(object sender, EventArgs e)
         {
             labelTitle.Text = "Prev clicked";
+            textDescription.Text = "This is the description when prev is clicked";
+            imageCourse.Image = UIImage.FromBundle("ps_top_card_01");
         }
 
         private void buttonNext_TouchUpInside(object sender, EventArgs e)
         {
             labelTitle.Text = "Next clicked";
+            textDescription.Text = "This is the description when next is clicked";
+            imageCourse.Image = UIImage.FromBundle("ps_top_card_02");
         }
 
     }
